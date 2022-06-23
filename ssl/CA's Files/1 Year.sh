5c25954910e784c7fbedf5599d4bd0deba8da97f
Shubham Trivedi <shubham.maskoid@gmail.com>
2022-06-23 12:57:33 +0530

    update

---
+++ ssl/CA's Files/1 Year.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p 'Enter Output CA Filename without Extension: ' caFileName
+read -p 'Enter CA Name (eg. My Home):' CANAME
+printf "\n\n (+) Generating CA Key"
+#for some reason windows 10 displays password on prompt, so hiding.. 
+stty -echo
+openssl genrsa -des3 -out ${caFileName}.key 2048
+stty echo
+printf "\n\n (+) Generating Root Certificate"
+stty -echo
+openssl req -x509 -new -nodes -key ${caFileName}.key -sha256 -days 365 -subj "/C=IN/ST=Gujarat/L=Vadodara/CN=${CANAME}" -out ${caFileName}.pem
+stty echo
---
+++ ssl/SSL Files/1 Year.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+read -p "Enter Common Name (Site Name, eg. something.test.home): " domainName
+read -p "Enter CA FileName: " caFileName
+openssl genrsa -out ${domainName}.key 2048
+
+openssl req -new -key ${domainName}.key -subj "/C=IN/ST=Gujarat/L=Vadodara/CN=${domainName}" -out ${domainName}.csr
+
+cat <<EOF >> ${domainName}.cnf
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = ${domainName}
+EOF
+
+
+#openssl v1.1  > supports this below
+#openssl x509 -req -in ${domainName}.csr -CA ${caFileName}.pem -CAkey ${caFileName}.key -CAcreateserial -out ${domainName}.crt -days 1825 -sha256 -addext "subjectAltName=DNS:${domainName}"
+#but for openssl < 1.1
+stty -echo 
+openssl x509 -req -in ${domainName}.csr -CA ${caFileName}.pem -CAkey ${caFileName}.key -CAcreateserial -out ${domainName}.crt -days 365 -sha256 -extfile ${domainName}.cnf
+stty echo 
+#cleanup
+rm ${domainName}.cnf
